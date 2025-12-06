9cb46419904da0d9f21b19ebfa1e1ea80b8f3fd5
PhilipBiechl <philip.biechl@gmail.com>
2025-12-06 16:48:22 +0100

    Path fixes

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,12 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  assetPrefix: isProd ? '/about-me/' : '',
-  basePath: isProd ? '/about-me' : '',
+  assetPrefix: '/about-me/',
+  basePath: '/about-me',
   output: 'export'
 };
 

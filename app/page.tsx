367e8a53bdcbd316c2d1c05fc80c49226de73b54
PhilipBiechl <philip.biechl@gmail.com>
2025-12-12 21:09:53 +0100

    redimensioning components

--- app/page.tsx
+++ app/page.tsx
@@ -16,17 +16,14 @@ export default function Home() {
         patternScale={3}
         patternDensity={1.2}
         pixelSizeJitter={0.5}
-        enableRipples
         rippleSpeed={0.4}
         rippleThickness={0.12}
         rippleIntensityScale={1.5}
-        liquid
         liquidStrength={0.12}
         liquidRadius={1.2}
         liquidWobbleSpeed={5}
         speed={0.6}
         edgeFade={0.25}
-        transparent
       />
       <div/>
 

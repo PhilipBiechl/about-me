c58db3c3538f02abc7d749346985605301c2b363
PhilipBiechl <philip.biechl@gmail.com>
2025-12-12 22:12:22 +0100

    different specification

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
       />
       <div/>
 
-      <div className={`font-sans relative flex flex-col`}>
+      <div className={`font-sans absolute`}>
       {/* Navbar */}
       <header className={`flex justify-between items-center px-10 py-6 ${dark ? "bg-gray-800" : "bg-white"} shadow-md sticky top-0 z-10`}>
       <div className="font-bold text-lg">Philip Biechl</div>
